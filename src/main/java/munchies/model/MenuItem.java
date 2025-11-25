00b1c55bac8b71a938420fc38e0293af28fd9756
Nokulunga (Noki) Motsweni <nokulunga.motsweni@praguecollege.cz>
2025-11-25 16:53:26 +0100

    Adding fields for ID, name, optional description of the MenuItem, price of the MenuItem and availability.

--- src/main/java/munchies/model/MenuItem.java
+++ src/main/java/munchies/model/MenuItem.java
@@ -3,5 +3,9 @@ package munchies.model;
 import java.math.BigDecimal;
 
 public class MenuItem {
-    // To be added later according to the project plan.
+    private String id; // MenuItem unique identifier
+    private String name; // e.g "Cheese Burger"
+    private String description; // May or may not be added depending on the restaurant
+    private BigDecimal price; // The cost of the MenuItem
+    private boolean available; // Shows the availability of the itm
 }
