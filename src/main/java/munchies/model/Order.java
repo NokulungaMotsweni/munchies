4ee871e1fab10442b04fc942c89f2afc8d545606
Nokulunga (Noki) Motsweni <nokulunga.motsweni@praguecollege.cz>
2025-11-25 18:01:00 +0100

    Addition of comments to Order.

--- src/main/java/munchies/model/Order.java
+++ src/main/java/munchies/model/Order.java
@@ -6,27 +6,31 @@ import java.util.List;
 
 public class Order {
 
-    private final String orderId;
-    private final List<OrderItem> items = new ArrayList<>();
-    private OrderStatus status = OrderStatus.NEW;
+    private final String orderId;                   // Unique identifier for the order
+    private final List<OrderItem> items = new ArrayList<>(); // Items that have been added to the order
+    private OrderStatus status = OrderStatus.NEW; // Current lifecyle state of the order
 
     public Order(String orderId) {
-        this.orderId = orderId;
+        this.orderId = orderId;                 // Assigns the order an identifier
     }
 
     public List<OrderItem> getItems() {
+        // Returns a copy of the items to prevent outside classes from changing the internal list
         return new ArrayList<>(items);
     }
 
     public void addItem(OrderItem item) {
+        // Adds an OrderItem to the order
         items.add(item);
     }
 
     public OrderStatus getStatus() {
+        // Returns the current status if the order
         return status;
     }
 
     public void setStatus(OrderStatus status) {
+        // Updates the order status (used by the Observer Pattern later (Dren))
         this.status = status;
     }
 
