3863ce200be4b7a7a1d198f0266dcf59b0321bf1
Nokulunga (Noki) Motsweni <nokulunga.motsweni@praguecollege.cz>
2025-12-02 22:24:49 +0100

    Add ToppingDecorator that implements Dish to support topping extensions.

--- src/main/java/munchies/model/toppings/ToppingDecorator.java
+++ src/main/java/munchies/model/toppings/ToppingDecorator.java
@@ -1,4 +1,33 @@
 package munchies.model.toppings;
 
-public class ToppingDecorator {
+import munchies.model.Dish;
+
+import java.math.BigDecimal;
+
+/**
+ * A base class for toppings that wrap another Dish.
+ * Provides default behaviour by delegating to the wrapped dish.
+ */
+
+public abstract class ToppingDecorator implements Dish {
+
+    // The Dish being wrapped by the ToppingDecorator
+    protected final Dish dish;
+
+    // Creates a ToppingDecorator
+    public ToppingDecorator(Dish dish) {
+        this.dish = dish;
+    }
+
+    // Returns the dish name
+    @Override
+    public String getName() {
+        return dish.getName();
+    }
+
+    // Returns the dish price in CZK
+    @Override
+    public BigDecimal getPrice() {
+        return dish.getPrice();
+    }
 }
