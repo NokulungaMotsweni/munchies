b15611304a0db345dda040f9c369e541312c8929
Nokulunga (Noki) Motsweni <nokulunga.motsweni@praguecollege.cz>
2025-12-13 00:22:22 +0200

    Remove the adding the topping price in the Bacon decorator class.

--- src/main/java/munchies/model/toppings/Bacon.java
+++ src/main/java/munchies/model/toppings/Bacon.java
@@ -6,7 +6,7 @@ import java.math.BigDecimal;
 
 public class Bacon  extends ToppingDecorator {
 
-    private final BigDecimal toppingPrice = new BigDecimal("25.00");
+
 
     public Bacon(Dish dish) {
         super(dish);
